5eb93ee1c6ec760f65ec6199eb8b49422c1938ae
Ansa-Dhedhi <ansadhedhi98@gmail.com>
2021-01-26 18:10:57 +0500

    test-case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/hello world/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('abc', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/hello piaic/i);
   expect(linkElement).toBeInTheDocument();
 });
